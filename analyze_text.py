2ec4e2e9ef6da759e0f634055047be4902c6e8e3
Jenell-w <49175487+Jenell-w@users.noreply.github.com>
2019-04-25 18:20:31 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -7,6 +7,6 @@ def analyze_text(text):
           letter_count += 1  
   num_e = text.count("e") + text.count("E")
   percent_e = ((num_e) / (letter_count))
-  return "The text contains " + str(letter_count) + " alphabetic characters, of which " + str(num_e) + " " + str((format(percent_e, '.2%'))) + " are 'e'."
+  return "The text contains " + str(letter_count) + " alphabetic characters, of which " + str(num_e) + " (" + str((format(percent_e, '.2%'))) + ") are 'e'."
 
     # Your code here
